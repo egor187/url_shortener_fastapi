ca2a3ae9760ad13d4d017e5f70f260a5917be84d
Yehor Osipovsky <e.osipovsky@volsor.com>
2022-06-05 15:23:28 +0300

    sqlalchemy models

---
+++ core/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+
+from .db import engine
+from .helpers import get_short_code
+
+Base = declarative_base()
+
+
+class URL(Base):
+    __tablename__ = 'URL'
+
+    id = Column(Integer, primary_key=True)
+    created = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    long_url = Column(String, nullable=False)
+    code = Column(String, default=get_short_code, nullable=False)
+
+    # def __repr__(self):
+    #     return f'URL instance: {self.created}: {self.long_url}: {self.code}'
+
+
+Base.metadata.create_all(engine)
